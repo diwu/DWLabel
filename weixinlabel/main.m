940635638f45465409c94fea1a144911fd514702
Di Wu <diwufet@gmail.com>
2012-02-21 23:32:20 +0800

    Initial Commit

---
+++ weixinlabel/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  weixinlabel
+//
+//  Created by Di Wu on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DIWUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DIWUAppDelegate class]));
+    }
+}
